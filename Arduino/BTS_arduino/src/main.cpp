5d50e7e46be1e5fa38fd51501c68b9e5e062bc54
surajrvind <surajrvind@gmail.com>
2026-04-01 17:46:44 +0100

    fixing broken merge

--- Arduino/BTS_arduino/src/main.cpp
+++ Arduino/BTS_arduino/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-<<<<<<< HEAD
 #include "steering.h"
 #include "motor_commands.h"
 
@@ -11,27 +10,11 @@ void setup(){
     setSteering(0.0);
     setThrottle(0);
     updateMotor();
-=======
-#include "servo_commands.h"
-#include "motor_commands.h"
-
-int x;
-
-void setup(){
-    setupMotor();
-    setupServo();
-    Serial.begin(9600);
-    Serial.setTimeout(1);
-
-    centre();
-    stop();
->>>>>>> a82512f53b9c5796fb3547d44d7b6ae81869d091
 }
 
 void loop(){
 
     if (Serial.available() > 0){
-<<<<<<< HEAD
     String msg = Serial.readStringUntil('\n');
 
     int throttle = 0;
@@ -48,35 +31,4 @@ void loop(){
     }
 }
 updateMotor();
-=======
-        char key = Serial.read();
-
-        switch (key){
-            case 'w':
-                throttle();
-                Serial.println("Throttle...");
-                break;
-            case 'a':
-                left();
-                Serial.println("Going left...");
-                break;
-            case 's':
-                reverse();
-                Serial.println("Reversing...");
-                break;
-            case 'd':
-                right();
-                Serial.println("Going right...");
-                break;
-            case 'x':
-                brake_f();
-                Serial.println("HOL UP");
-                break;
-            case 'c':
-                centre();
-                Serial.println("Straightening...");
-                break;
-        }
-    }
->>>>>>> a82512f53b9c5796fb3547d44d7b6ae81869d091
 }
